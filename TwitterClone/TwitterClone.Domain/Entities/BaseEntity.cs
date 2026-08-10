ee569e4e89d28fe5ffe15dcbfa566d3b7218e28d
Nazmul Islam <nazmulislam93400@gmail.com>
2026-08-10 08:37:34 +0600

    fix: complete BaseEntity implementation

--- TwitterClone/TwitterClone.Domain/Entities/BaseEntity.cs
+++ TwitterClone/TwitterClone.Domain/Entities/BaseEntity.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace TwitterClone.Domain.Entities
 {
-    internal class BaseEntity
+    public class BaseEntity
     {
+        private Guid _id { get; }
+        private DateTime _createdAt { get; }
+        private DateTime? _modifiedAt { get; set; }
+        private Guid _createdBy { get; set; }
+        private Guid? _modifiedBy { get; set; }
+
+
+        public BaseEntity(Guid id)
+        {
+            _id = id;
+            _createdAt = DateTime.UtcNow;
+        }
     }
 }
