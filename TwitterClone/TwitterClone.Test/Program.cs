a27f6ae9bd501a357f4634a2cdb67e2c90f1ec7f
Nazmul Islam <nazmulislam93400@gmail.com>
2026-08-18 21:52:06 +0600

    test: add unit test project for domain library

---
+++ TwitterClone/TwitterClone.Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using TwitterClone.Domain.Entities;
+
+//var notification = new CommentNotification(Guid.NewGuid()) ;
+//Console.WriteLine(notification.GetMessage());
+
+var notifications = new List<Notification>  //Upcusting
+{
+    new LikeNotification(Guid.NewGuid()),
+    new CommentNotification(Guid.NewGuid()),
+    new FriendRequestNotification(Guid.NewGuid()),
+    new MentionNotification(Guid.NewGuid()),
+    new SystemNotification()
+};
+
+foreach (var notifiaction in notifications)
+{
+    Console.WriteLine(notifiaction.GetMessage());
+    Console.WriteLine();
+}
+
